fd76aee6b479801e912b565ad96b9359e1881c77
repl.it user <replituser@example.com>
2020-10-13 10:47:59 +0000

    ew

--- myprogram.c
+++ myprogram.c
@@ -23,39 +23,76 @@ for (int i=0;i<count;++i)	{
 }
 
 int max_of_numbers(int *array, int count)
-{
+{int rem=0;
+ for (int i=0;i<count;++i){
+    if(array[i]>array[rem]){
+rem=i;
+
+    
+		//TODO: only add even numbers, e.g., 4. Skip odd numbers, e.g., 3
+    }
+	}
+
 	//TODO: return the maximum number from the array
-	return array[0];
+	return array[rem];
 }
 
 int reversed_number(int number)
 {
-	int reversed = number;
-
-	//TODO: if input is 12345, return 54321
+	
+int reversed=0;
+int toAdd;
+while(number!=0){
+toAdd=number%10;
+number=number/10;
+reversed=reversed*10+toAdd;
+}	//TODO: if input is 12345, return 54321
 
 	return reversed;
 }
 
 int is_prime(int number)
-{
-	//TODO: return 1 if the input number is prime, otherwise 0
+{ int starter= 2;
+  if(number==2 ){
+   return 1;
+  }else if (number == 1 ){
+   return 0;
+  }else{
+ 
+while(number%starter!=0){
+starter++;
+}
+}if(starter!=number){
+  return 0;
+} else{ 
 
-	return 0;
+	//TODO: return 1 if the input number is prime, otherwise 0
+	return 1;
+}
 }
 
 int count_primes(int start, int end)
-{
+{ 
+  int count=0;
 	//TODO: return the count of prime numbers in range [start, end] inclusive.
-
-	return 0;
+  for(int i = start; i<=end;i++) {
+if(is_prime(i) ){
+  count++;
+}
+}
+	return count;
 }
 
 
 char alphabet_index(int index)
 {
+if(0<=index<=25){
+	char*a ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	//TODO: for index 0, return A. index 1, B, etc. until 25 for Z.
 	//if index is out of range, return space ' '.
-	return 'A';
+	return  a[index];}
+  else{
+		return ' ';
+  }
 }
 
